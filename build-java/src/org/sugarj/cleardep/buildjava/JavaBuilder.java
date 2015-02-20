0dd6d3be5a4b54cbac4d518c684450aa59d03524
Manuel Weiel <manuel@weiel.eu>
2015-02-20 12:11:42 +0100

    updated JavaBuilder (untested!)

--- build-java/src/org/sugarj/cleardep/buildjava/JavaBuilder.java
+++ build-java/src/org/sugarj/cleardep/buildjava/JavaBuilder.java
@@ -1,39 +1,41 @@
 package org.sugarj.cleardep.buildjava;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
-import org.sugarj.cleardep.CompilationUnit;
 import org.sugarj.cleardep.CompilationUnit.State;
 import org.sugarj.cleardep.SimpleCompilationUnit;
-import org.sugarj.cleardep.SimpleMode;
 import org.sugarj.cleardep.build.Builder;
 import org.sugarj.cleardep.build.BuilderFactory;
 import org.sugarj.cleardep.stamp.LastModifiedStamper;
 import org.sugarj.cleardep.stamp.Stamper;
 import org.sugarj.common.FileCommands;
+import org.sugarj.common.JavaCommands;
 import org.sugarj.common.errors.SourceCodeException;
 import org.sugarj.common.errors.SourceLocation;
 import org.sugarj.common.path.Path;
 import org.sugarj.common.path.RelativePath;
 import org.sugarj.common.util.Pair;
-import org.sugarj.common.JavaCommands;
 
-public class JavaBuilder extends Builder<JavaBuildContext, List<Path>, SimpleCompilationUnit> {
+public class JavaBuilder extends Builder<JavaBuildContext, JavaBuilder.Input, SimpleCompilationUnit> {
 
-	public static BuilderFactory<JavaBuildContext, List<Path>, SimpleCompilationUnit, JavaBuilder> factory = new BuilderFactory<JavaBuildContext, List<Path>, SimpleCompilationUnit, JavaBuilder>() {
+	public static BuilderFactory<JavaBuildContext, Input, SimpleCompilationUnit, JavaBuilder> factory = new BuilderFactory<JavaBuildContext, Input, SimpleCompilationUnit, JavaBuilder>() {
 		@Override
 		public JavaBuilder makeBuilder(JavaBuildContext context) { return new JavaBuilder(context); }
 	};
 	
 	public static class Input {
-		public final RelativePath ppInput;
-		public final RelativePath ppTermOutput;
-		public Input(RelativePath ppInput, RelativePath ppTermOutput) {
-			this.ppInput = ppInput;
-			this.ppTermOutput = ppTermOutput;
+		public final List<Path> inputFiles;
+		public final Path targetDir;
+		public final Path sourcePath;
+		public final List<Path> classPaths;
+		public final List<RequirableCompilationUnit> requiredUnits;
+		public Input(List<Path> inputFiles, Path targetDir, Path sourcePath, List<Path> classPaths, List<RequirableCompilationUnit> requiredUnits) {
+			this.inputFiles = inputFiles;
+			this.targetDir = targetDir;
+			this.sourcePath = sourcePath;
+			this.classPaths = classPaths;
+			this.requiredUnits = requiredUnits;
 		}
 	}
 	
@@ -42,13 +44,13 @@ public class JavaBuilder extends Builder<JavaBuildContext, List<Path>, SimpleCom
 	}
 	
 	@Override
-	protected String taskDescription(List<Path> input) {
+	protected String taskDescription(Input input) {
 		return "Compile Java files";
 	}
 	
 	@Override
-	protected Path persistentPath(List<Path> input) {
-		return new RelativePath(context.baseDir, FileCommands.tryGetRelativePath(context.binPath) + ".dep");
+	protected Path persistentPath(Input input) {
+		return new RelativePath(context.baseDir, FileCommands.tryGetRelativePath(input.targetDir) + ".dep");
 	}
 
 	@Override
@@ -60,12 +62,12 @@ public class JavaBuilder extends Builder<JavaBuildContext, List<Path>, SimpleCom
 	public Stamper defaultStamper() { return LastModifiedStamper.instance; }
 
 	@Override
-	public void build(SimpleCompilationUnit result, List<Path> input) throws IOException {
+	public void build(SimpleCompilationUnit result, Input input) throws IOException {
 		try {
-			for (Path p: input) {
+			for (Path p: input.inputFiles) {
 				result.addSourceArtifact(FileCommands.getRelativePath(context.baseDir, p));
 			}
-			List<Path> outFiles = JavaCommands.javac(input, context.binPath, new ArrayList<Path>());
+			List<Path> outFiles = JavaCommands.javac(input.inputFiles, input.targetDir, input.sourcePath, input.classPaths);
 			for (Path p: outFiles) {
 				result.addGeneratedFile(p);
 			}
