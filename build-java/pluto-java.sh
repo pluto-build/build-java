0241815fb1ed2a4fc3c522096239df28f96dc790
Sebastian Erdweg <seba.github@gmail.com>
2015-09-03 11:01:42 -0700

    fixed starting script

--- build-java/pluto-java.sh
+++ build-java/pluto-java.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-ARGS="pluto-java build.pluto.buildjava.JavaBuilder.factory build.pluto.buildjava.JavaBuilder\$Input $@"
+ARGS="pluto-java build.pluto.buildjava.JavaBuilder.factory build.pluto.buildjava.JavaInput $@"
 
 mvn compile exec:java -Dexec.args="$ARGS"
